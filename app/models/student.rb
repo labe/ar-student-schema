6cab319b13aaadd27d17e915c0939d0521b30c45
Lora <lora.abe@gmail.com>
2013-05-09 20:50:51 -0500

    initial commit

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,18 @@
 require_relative '../../db/config'
+require 'date'
 
-# implement your Student model here
+class Student < ActiveRecord::Base
+
+	validates :email, :format => { :with => /.+@.+\..{2,}/ }, :uniqueness => true
+	validates :age, :numericality => { :greater_than_or_equal_to => 5 }
+	validates :phone, :format => { :with => /.*\d{1}.*\d{3}.*\d{3}.*\d{4}.*/ }
+	
+	def name
+		first_name + " " + last_name
+	end
+
+	def age
+		(DateTime.now - birthday) / 365
+	end
+
+end
