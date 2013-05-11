2e8f122d645fa52d090b626aa5cb3911536d0eda
Lora <lora.abe@gmail.com>
2013-05-11 18:41:17 -0500

    Revert "Revert "initial commit""
    This reverts commit 250b3bf0f56db37d4486a678398137253a2e2333.

--- app/models/students_and_teachers.rb
+++
@@ -1,2 +0,0 @@
-class StudentsAndTeachers < ActiveRecord::Base
-end
--- app/models/teacher.rb
+++
@@ -1,7 +0,0 @@
-class Teacher < ActiveRecord::Base
-	validates :email, :format => { :with => /\S+@\S+\.\w{2,}/ }, :uniqueness => true
-	validates :name, :uniqueness => true
-
-	# belongs_to :student
-	
-end
